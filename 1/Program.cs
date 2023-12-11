c0a05858c0c7718be8f5797ff59def9c2524982a
elena <princessa-len4ik@mail.ru>
2023-12-11 22:18:57 +0500

    Решение первой задачи

---
+++ 1/Program.cs
@@ -0,0 +1,39 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        matrix[i, j] = new Random().Next(1,10);
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + "\t");
+            Console.WriteLine();
+        }
+}
+
+void RMatrix(int[,] matrix)
+{
+Console.Write("Введите индекс элемента: ");
+int[] i = Console.ReadLine().Split(" ").Select(y => int.Parse(y)).ToArray();
+        if (i[0] > matrix.GetLength(0) || i[1] > matrix.GetLength(1))
+    {
+        Console.WriteLine("Такого элемента нет");
+    }
+        else
+    {
+        Console.WriteLine(matrix[i[0], i[1]]);
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+RMatrix(matrix);
+
